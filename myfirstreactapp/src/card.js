270deeff173f45490cd9fecf39615eac2b38afd5
Henok Belachew <henokbelachewstu@gmail.com>
2022-12-11 06:55:26 +0300

    3 componenet created

--- myfirstreactapp/src/card.js
+++ myfirstreactapp/src/card.js
@@ -1,23 +1,22 @@
 import './card.css';
+import ReactDOM from 'react-dom/client';
 import IMG from './images/image-victor.jpg';
 import headerIMG from './images/bg-pattern-card.svg'
 
-function card() {
+
+
+function Header() {
   return (
-  
-    <div className='container'>
-      
+    <div className='header'>
       <img id='headerimg' src={headerIMG} alt="" />
       <img id='img' src={IMG} alt="" />
+    </div>
+  )
+}
 
-      <div id='userinfo'>
-        <h2>Vitor Crest <span>26</span></h2>
-        <span>London</span>
-      </div>
-
-      <div id="line"></div>
-     
-      <div className='lower' id='lowerr'>
+function Lower() {
+  return (
+    <div className='lower' id='lowerr'>
           <div className="info">
             <h2>80k</h2>
             <span className="tag">Followers</span>
@@ -31,11 +30,41 @@ function card() {
             <span className="tag">Photos</span>
           </div>
       </div>
-        
+  )
+}
+
+function MainContent() {
+  return (
+    <div className='main'>
+        <div id='userinfo'>
+          <h2>Vitor Crest <span>26</span></h2>
+          <span>London</span>
+        </div>
+
+        <div id="line"></div>
+      </div>
+  ) 
+}
+function Card() {
+  return (
+  
+    <div className='container'>
+      
+      <Header />
+
+      <MainContent />
+           
+      <Lower />
 
     </div>
     
   );
 }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <Card />
 
-export default card;
+);
+
+// export default card;
--- myfirstreactapp/src/index.js
+++ myfirstreactapp/src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import Card from './card';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <Card />
 
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
