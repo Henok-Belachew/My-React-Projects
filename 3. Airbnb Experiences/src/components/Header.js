6030ba8b74fb12293fbee296118118995415be48
Henok Belachew <henokbelachewstu@gmail.com>
2023-01-20 07:57:44 +0300

    made final touch on the style

--- 3. Airbnb Experiences/src/components/Header.js
+++ 3. Airbnb Experiences/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header () {
     return (
         <div className="header">     
             <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led  by one-of-a-kind <br/> hosts—all without leaving home.
+            <p>Join unique interactive activities led  by one-of-a-kind hosts—all without leaving home.
             </p>
         </div>
 
